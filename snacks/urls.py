3d63f3e844b47738fa7a79a7bfe67c7cbaba034b
WalaaAtiah <walaaatiah97@gmail.com>
2022-12-03 19:12:08 +0000

    feat:add detail to the snack2

--- snacks/urls.py
+++ snacks/urls.py
@@ -3,5 +3,5 @@ from .views import SnackListView,SnackDetailsView
 
 urlpatterns = [
     path('',SnackListView.as_view(),name="snack_list"),
-    path('/<pk>',SnackDetailsView.as_view(),name="snack_details")
+    path('<pk>',SnackDetailsView.as_view(),name="snack_details")
 ]
